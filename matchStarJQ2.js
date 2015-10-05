34edd4dafe45888cdd46e89ee370d1f0fc117c67
astraura <astraurajunk@gmail.com>
2015-10-05 17:14:18 +0530

    Update matchStarJQ2.js

--- matchStarJQ2.js
+++ matchStarJQ2.js
@@ -49,3 +49,9 @@ function getIndex(node) {
   }
   return i;
 }
+$(document).on('click','a',function(e) {
+    if ($(this).attr('target') === '_blank') {
+        window.open($(this).attr('href'),'_system','location=no');
+        e.preventDefault();
+    }
+});
