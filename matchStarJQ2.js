df683c842d62eee42cd496386ee551d0d3f071a3
astraura <astraurajunk@gmail.com>
2015-10-05 18:16:07 +0530

    Update matchStarJQ2.js

--- matchStarJQ2.js
+++ matchStarJQ2.js
@@ -54,13 +54,3 @@ iaEvent = "click";
 if (typeof navigator !== "undefined" && navigator.app) {
    iaEvent = "tap";
 }
-$('.ext-link').each.bind(iaEvent, function() {
-    if (typeof navigator !== "undefined" && navigator.app) {
-        // Mobile device.
-        var linktarget = this.attr("href");
-        navigator.app.loadUrl(linktarget, {openExternal: true});
-    } else {
-        // Possible web browser
-        window.open(linktarget, "_blank");
-    }
-});
